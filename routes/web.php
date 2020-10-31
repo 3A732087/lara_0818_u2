251a13ddac3ddbff585e7b3efa0cd40a81312597
Xu jia quan <quannn0180716v@gmail.com>
2020-10-31 15:55:30 +0800

    補route命名

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('hello/{name?}',[HelloController::class,'index']);
+Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
