46e55922caf7fac48ca02c8f3224c0931532c8be
Xu jia quan <quannn0180716v@gmail.com>
2020-11-29 13:20:38 +0800

    更改route去串接HomeController(作業錯誤更正)

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/',function (){
+    return view('welcome');
+});
 
+Route::get('home',[HomeController::class, 'index'])->name('home.index');
 
 Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
+
+
